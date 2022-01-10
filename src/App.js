13e5ac869dcf9c4bdb0e741a76090480bffd882f
cheveanrichards <richardsca7@outlook.com>
2022-01-10 00:38:57 -0500

    interfaces using taiwindscss UI package

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//react-icons more efficient than fontawesome
+import { BiArchive } from 'react-icons/bi'
+import Search from './components/Search';
+function App() {
+  return (
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl"><BiArchive className='inline-block text-red-400'/>Your Appointments</h1>
+      <Search/>
+    </div>
+  );
+}
+
+export default App;
